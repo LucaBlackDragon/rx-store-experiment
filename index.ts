1402f714eab618154b64f790a1cda99fd132062e
LucaBlackDragon <luca.nardi@live.it>
2021-01-19 10:40:06 +0100

    refactoring id elementi

--- index.ts
+++ index.ts
@@ -3,17 +3,21 @@ import { debounceTime } from "rxjs/operators";
 import { dispatch, store$ } from "./store";
 import { genericButtonClick, resetClick } from "./actions";
 
+const genericButton = document.querySelector("#generic-btn");
+const resetButton = document.querySelector("#reset-btn");
+const clicksCounter = document.querySelector("#clicks-counter");
+
 store$.subscribe(state => {
-  document.querySelector("#clicks").innerHTML = state.counter.toString();
+  clicksCounter.innerHTML = state.counter.toString();
 });
 
 const reset = () => dispatch(resetClick());
 
-fromEvent(document.querySelector("#btn"), "click")
+fromEvent(genericButton, "click")
   .pipe(debounceTime(60))
   .subscribe(() => dispatch(genericButtonClick()));
 
-fromEvent(document.querySelector("#reset"), "click").subscribe(() => reset());
+fromEvent(resetButton, "click").subscribe(() => reset());
 
 // needed to initialize/"trigger" the store reducer
 reset();
